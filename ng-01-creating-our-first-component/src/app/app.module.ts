78e4d8465b43e98c92167365d39222c8b131a642
Samuel <samuelbrandao@gmail.com>
2019-09-23 00:12:09 -0300

    Lecture 25

--- ng-01-creating-our-first-component/src/app/app.module.ts
+++ ng-01-creating-our-first-component/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PersonsComponent } from './persons/persons.component';
+import { PersonsInputComponent } from './persons/persons-input.component';
 
 @NgModule({
-  declarations: [AppComponent, PersonsComponent],
+  declarations: [AppComponent, PersonsComponent, PersonsInputComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
--- ng-01-creating-our-first-component/src/app/persons/persons-input.component.ts
+++ ng-01-creating-our-first-component/src/app/persons/persons-input.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-persons-input',
-  templateUrl: './persons.component.html'
+  templateUrl: './persons-input.component.html',
+  styleUrls: ['./persons-input.component.css']
 })
 export class PersonsInputComponent {
   @Input() personList: string[];
