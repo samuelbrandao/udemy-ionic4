1852f9d532350a70ff537c637e7cd4c5a50ad497
Samuel <samuelbrandao@gmail.com>
2019-10-14 12:39:23 -0300

    lecture56

--- cmp-basics-03-added-validation-logic/app.js
+++ cmp-basics-03-added-validation-logic/app.js
@@ -2,6 +2,15 @@ const reasonInput = document.querySelector('#input-reason');
 const amountInput = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
+const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
+
+let totalExpenses = 0;
+
+const clear = () => {
+  reasonInput.value = '';
+  amountInput.value = '';
+}
 
 confirmBtn.addEventListener('click', () => {
   const enteredReason = reasonInput.value;
@@ -14,5 +23,15 @@ confirmBtn.addEventListener('click', () => {
   ) {
     return;
   }
-  console.log(enteredReason, enteredAmount);
+  //console.log(enteredReason, enteredAmount);
+  const newItem = document.createElement('ion-item');
+  newItem.textContent = enteredReason + ': $' + enteredAmount;
+  
+  expensesList.appendChild(newItem);
+
+  totalExpenses += enteredAmount;
+  totalExpensesOutput.textContent = totalExpenses;
+  clear();
 });
+
+cancelBtn.addEventListener('click', clear);
