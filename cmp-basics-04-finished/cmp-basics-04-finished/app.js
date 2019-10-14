51a6188360078e302013d8e4ec8324a8517149c0
Samuel <samuelbrandao@gmail.com>
2019-10-14 15:04:07 -0300

    Lecture 60

--- cmp-basics-04-finished/cmp-basics-04-finished/app.js
+++ cmp-basics-04-finished/cmp-basics-04-finished/app.js
@@ -4,14 +4,14 @@ const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
 const expensesList = document.querySelector('#expenses-list');
 const totalExpensesOutput = document.querySelector('#total-expenses');
-const alertCtrl = document.querySelector('ion-alert-controller')
+const alertCtrl = document.querySelector('ion-alert-controller');
 
 let totalExpenses = 0;
 
 const clear = () => {
   reasonInput.value = '';
   amountInput.value = '';
-}
+};
 
 confirmBtn.addEventListener('click', () => {
   const enteredReason = reasonInput.value;
@@ -22,15 +22,23 @@ confirmBtn.addEventListener('click', () => {
     enteredAmount <= 0 ||
     enteredAmount.trim().length <= 0
   ) {
+    alertCtrl
+      .create({
+        message: 'Please enter valid reason and amount!',
+        header: 'Invalid inputs',
+        buttons: ['Okay']
+      })
+      .then(alertElement => {
+        alertElement.present();
+      });
     return;
   }
-  //console.log(enteredReason, enteredAmount);
   const newItem = document.createElement('ion-item');
   newItem.textContent = enteredReason + ': $' + enteredAmount;
-  
+
   expensesList.appendChild(newItem);
 
-  totalExpenses += enteredAmount;
+  totalExpenses += +enteredAmount;
   totalExpensesOutput.textContent = totalExpenses;
   clear();
 });
