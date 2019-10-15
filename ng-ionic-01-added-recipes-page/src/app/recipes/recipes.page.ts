20d61629783cfbe0ca74aee9878dbf5b0ec14e6a
Samuel <samuelbrandao@gmail.com>
2019-10-15 12:48:20 -0300

    Lecture 68

--- ng-ionic-01-added-recipes-page/src/app/recipes/recipes.page.ts
+++ ng-ionic-01-added-recipes-page/src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl1zn_9nQe-1Dg22PFK670s8MaiNUM_Qwynot9_D-XU1sDCNV4',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://photos.bigoven.com/recipe/hero/spaghetti-f48f9b.jpg?h=300&w=300',
+      ingredients: ['Spaghetti', 'Meat', 'Tomattoes']
+    }
+  ];
 
   constructor() { }
 
